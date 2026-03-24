194b6d925d08f4c3cef47c93284216896de8d059
Subhankar Sourav <souravsubhankar1706@gmail.com>
2026-03-24 10:40:02 +0530

    Clean version with env variables and local MySQL setup

--- database.py
+++ database.py
@@ -1,19 +1,19 @@
 import os
 import mysql.connector
-from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conn = None
 cursor = None
 
 try:
-    url = urlparse(os.getenv("MYSQL_URL"))
-
     conn = mysql.connector.connect(
-        host=url.hostname,
-        user=url.username,
-        password=url.password,
-        database=url.path[1:],
-        port=url.port
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
     cursor = conn.cursor(dictionary=True)
